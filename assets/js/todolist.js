d0dc29505474956b196c4fd805c6ffbe5b5cfb78
Jonathan <jrinciari@or-specialties.com>
2017-08-10 18:39:43 -0400

    Added remove functionality

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -4,3 +4,10 @@ $("li").click(function (){
 	$(this).toggleClass("completed");
 });
 
+$("span").click(function (event){
+	$(this).parent().fadeOut(500,function (){
+		$(this).remove();
+	})
+	event.stopPropagation();
+})
+
